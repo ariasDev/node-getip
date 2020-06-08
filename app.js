dbfefbf3d27fe6280dda1a0052ff5a2b1d133b54
juanArias <juanariah@outlook.com>
2020-06-08 13:41:17 -0500

    env

--- app.js
+++ app.js
@@ -3,20 +3,12 @@ const app = express()
 const cors = require('cors')
 const fs = require('fs')
 const path = require('path');
+const dotenv = require('dotenv').config()
+
 
 app.use(cors())
-
-
-// app.use( (req, res, next) =>{
-//     console.log('Ejecutando mw de fs write');
-//     fs.appendFile('ips.txt', `\n new ip: ${req.connection.remoteAddress}`, function(err) {
-//         if(err) return console.error(err);
-//     });
-//     next()
-// })
-
 app.get('/', (req, res, next) => {
-    //res.send({"respose": "server okay"})
+    
     console.log('Ejecutando mw de fs write');
     fs.appendFile('ips.txt', `\n new ip: ${req.connection.remoteAddress}`, function(err) {
         if(err) return console.error(err);
@@ -26,10 +18,8 @@ app.get('/', (req, res, next) => {
 
 
 app.use(express.static(path.join(__dirname, 'Templates')));
-
-app.listen(3001, () => {
-    console.log(`app is running on port 3001`);
-    
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is running on port ${process.env.PORT}`);
 })
 
 module.exports = app
